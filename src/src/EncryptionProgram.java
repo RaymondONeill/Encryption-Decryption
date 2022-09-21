9d41d7a794df9c8879282e15ae15ed535bc8a27d
Raynond ONeill <oneillraymond71@gmail.com>
2022-09-21 01:20:30 -0700

    made the fix but I have no clue it just started working

--- src/src/EncryptionProgram.java
+++ src/src/EncryptionProgram.java
@@ -9,23 +9,21 @@ public class EncryptionProgram {
     private String line;
     private char[] letters;
 
-
-    EncryptionProgram() {
+    EncryptionProgram(){
         scanner = new Scanner(System.in);
         random = new Random();
-        list = new ArrayList();
-        shuffledList = new ArrayList();
+        list = new ArrayList<>();
+        shuffledList = new ArrayList<>();
         character = ' ';
 
         newKey();
         askQuestion();
     }
-
-    private void askQuestion() {
-        while (true) {
-            System.out.println("************************************************");
+    private void askQuestion(){
+        while(true) {
+            System.out.println("********************************************");
             System.out.println("What do you want to do?");
-            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit)");
+            System.out.println("(N)ewKey,(G)etKey,(E)ncrypt,(D)ecrypt,(Q)uit");
             char response = Character.toUpperCase(scanner.nextLine().charAt(0));
 
             switch (response) {
@@ -36,30 +34,27 @@ public class EncryptionProgram {
                 case 'Q' -> quit();
                 default -> System.out.println("Not a valid answer.");
             }
-
         }
     }
-
-    private void newKey() {
+    private void newKey(){
 
         character = ' ';
         list.clear();
         shuffledList.clear();
 
-        for (int i = 32; i < 127; i++);{
-            list.add(Character.valueOf(character));
+        for(int i = 32; i < 127; i++) {
+            list.add(character);
             character++;
         }
 
-        shuffledList = new ArrayList(list);
+        shuffledList = new ArrayList<>(list);
         Collections.shuffle(shuffledList);
-        System.out.println("*A new key has been generated.*");
+        System.out.println("*A new key has been generated*");
 
     }
-
-    private void getKey() {
+    private void getKey(){
         System.out.println("Key: ");
-        for (Character x : list) {
+        for(Character x : list) {
             System.out.print(x);
         }
         System.out.println();
@@ -67,19 +62,16 @@ public class EncryptionProgram {
             System.out.print(x);
         }
         System.out.println();
-
-
     }
-
     private void encrypt(){
         System.out.println("Enter a message to be encrypted: ");
         String message = scanner.nextLine();
 
         letters = message.toCharArray();
 
-        for(int i =0;i<letters.length;i++) {
+        for(int i = 0; i < letters.length; i++) {
 
-            for(int j =0;j<list.size();j++) {
+            for(int j = 0; j < list.size(); j++) {
                 if(letters[i] == list.get(j)) {
                     letters[i] = shuffledList.get(j);
                     break;
@@ -92,17 +84,17 @@ public class EncryptionProgram {
         }
         System.out.println();
     }
-
     private void decrypt(){
         System.out.println("Enter a message to be decrypted: ");
         String message = scanner.nextLine();
 
         letters = message.toCharArray();
 
-        for(int i =0;i<letters.length;i++) {
-            for(int j =0;j<shuffledList.size();j++) {
+        for(int i = 0; i < letters.length; i++) {
+
+            for(int j = 0; j < shuffledList.size(); j++) {
                 if(letters[i] == shuffledList.get(j)) {
-                    letters[i]=list.get(j);
+                    letters[i] = list.get(j);
                     break;
                 }
             }
@@ -113,10 +105,8 @@ public class EncryptionProgram {
         }
         System.out.println();
     }
-
-    private void quit() {
+    private void quit(){
         System.out.println("Program closing.");
         System.exit(0);
     }
 }
-
