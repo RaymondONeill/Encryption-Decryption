481196983501d664b5238dd4f51e95db95482e4f
Raynond ONeill <oneillraymond71@gmail.com>
2022-09-21 00:14:56 -0700

    I made the code work but for some reason it is not doing the soul purpose of why its made I cant get it to encrypt

--- src/src/EncryptionProgram.java
+++ src/src/EncryptionProgram.java
@@ -1,18 +1,13 @@
-import javax.xml.stream.events.Characters;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class EncryptionProgram {
     private Scanner scanner;
     private Random random;
     private ArrayList<Character> list;
-    private ArrayList<Characters> shuffledList;
+    private ArrayList<Character> shuffledList;
     private char character;
     private String line;
     private char[] letters;
-    private char[] secretLetters;
 
 
     EncryptionProgram() {
@@ -67,8 +62,8 @@ public class EncryptionProgram {
         for (Character x : list) {
             System.out.print(x);
         }
-        System.out.print();
-        for (Character x : shuffledList) {
+        System.out.println();
+        for(Character x : shuffledList) {
             System.out.print(x);
         }
         System.out.println();
@@ -76,54 +71,47 @@ public class EncryptionProgram {
 
     }
 
-    private void encrypt() {
-        System.out.println("Enter message to be encrypted: ");
+    private void encrypt(){
+        System.out.println("Enter a message to be encrypted: ");
         String message = scanner.nextLine();
 
         letters = message.toCharArray();
 
-        for (int i = 0; i < letters.length; i++) {
-            for (int j = 0; j < list.size(); j++) {
-                if (letters[i] == list.get(j)) {
-                    letters[i] == shuffledList.get(j);
+        for(int i =0;i<letters.length;i++) {
+
+            for(int j =0;j<list.size();j++) {
+                if(letters[i] == list.get(j)) {
+                    letters[i] = shuffledList.get(j);
                     break;
                 }
-
-
             }
         }
-
         System.out.println("Encrypted: ");
-        for (char x : letters) {
-            System.out.println(x);
+        for(char x : letters) {
+            System.out.print(x);
         }
         System.out.println();
-
     }
 
-    private void decrypt() {
-
-        System.out.println("Enter message to be decrypted: ");
+    private void decrypt(){
+        System.out.println("Enter a message to be decrypted: ");
         String message = scanner.nextLine();
 
         letters = message.toCharArray();
 
-        for (int i = 0; i < letters.length; i++) {
-            for (int j = 0; j < list.size(); j++) {
-                if (letters[i] == shuffledList.get(j)){
-                    letters[i] == list.get(j);
+        for(int i =0;i<letters.length;i++) {
+            for(int j =0;j<shuffledList.size();j++) {
+                if(letters[i] == shuffledList.get(j)) {
+                    letters[i]=list.get(j);
                     break;
                 }
-
             }
         }
-
         System.out.println("Decrypted: ");
-        for (char x : letters) {
-            System.out.println(x);
+        for(char x : letters) {
+            System.out.print(x);
         }
         System.out.println();
-
     }
 
     private void quit() {
